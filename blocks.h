17cbc5eca8ff6250a2487e206628938859b41ba2
Frederik Scheerer <s8fksche@stud.uni-saarland.de>
2020-11-23 23:04:38 +0100

    config

---
+++ blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
+#define WIDGET_PATH "/home/frederik/.zsh/scripts/status/"
+
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+  {"Volume: ", WIDGET_PATH "volume", 0, 1},
+	{"", "date '+%a %d %b %Y %H:%M'",					60,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
