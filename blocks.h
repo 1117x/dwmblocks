938ecaa8898fe24214724ec71fe4331a2a2e5d4a
Frederik Scheerer <s8fksche@stud.uni-saarland.de>
2020-11-24 12:17:14 +0100

    added status scripts

--- blocks.h
+++ blocks.h
@@ -1,12 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 
-#define WIDGET_PATH "/home/frederik/.zsh/scripts/status/"
+#define WIDGET_PATH "/home/frederik/bin/dwmblocks/status/"
 
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
   {"Volume: ", WIDGET_PATH "volume",          0,    1},
   {"",         WIDGET_PATH "battery",         10,   0},
+  {"",         WIDGET_PATH "bandwidth",       1,    0},
+  {"",         WIDGET_PATH "memory",          5,    0},
+  {"",         WIDGET_PATH "cpu",             5,    0},
   {"",         WIDGET_PATH "internet",        10,   0},
 	{"",         "date '+%a %d %b %Y %H:%M'",		60,		0},
 };
