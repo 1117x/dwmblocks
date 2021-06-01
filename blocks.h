51f796a49009ca1b97c4024ec58a7b1355751b54
Frederik Scheerer <s8fksche@stud.uni-saarland.de>
2021-06-01 23:24:34 +0200

    added mail widget

--- blocks.h
+++ blocks.h
@@ -7,6 +7,7 @@ static const Block blocks[] = {
 
   {" ",         WIDGET_PATH "volume",          60,   1},
   {"",         WIDGET_PATH "cmus_status",       60,   2},
+  {"",         WIDGET_PATH "mail",              60,   3},
   {"",         WIDGET_PATH "battery",           10,   0},
   /* {"",         WIDGET_PATH "bandwidth",       1,    0}, */
   /* {"",         WIDGET_PATH "memory",          5,    0}, */
