7443ce347f3ecc40b15accc7ce0ffb85e53e4c30
Frederik Scheerer <s8fksche@stud.uni-saarland.de>
2021-07-14 13:18:15 +0200

    bluetooth

--- blocks.h
+++ blocks.h
@@ -6,6 +6,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
   {" ",         WIDGET_PATH "volume",          60,   1},
+  {"",         "/home/frederik/.zsh/scripts/dmenu-bluetooth --status",
+                                                60,   1},
   /* {"",         WIDGET_PATH "cmus_status",       60,   2}, */
   {"",         WIDGET_PATH "mail",              60,   3},
   {"",         WIDGET_PATH "battery",           10,   0},
